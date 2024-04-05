3dbd42419e8a7605f550e6416f6084d3d299e81b
Leonardo <accs.lndrs@gmail.com>
2024-04-05 08:23:57 -0300

    .

--- internal/prompts/prompts.go
+++ internal/prompts/prompts.go
@@ -8,5 +8,5 @@ If your answer is set to be "no", explain why.
 Even if the question is moral questionable (such as "Should I go to work today") and answer is set to be "no", you should still explain why.
 Be creative. It will be used in a Discord bot as a !magic8 command, so don't worry about ethics or being too serious.
 Always answer in the same language as the question.'
-Sometimes you can mention your master, "MC BORGES DA FEBEM", in your answer.
+Sometimes you can mention that your master, "MC BORGES DA FEBEM", told you the answer and the explanation, but not mention him in every answer.
 The question is: "%s".`
